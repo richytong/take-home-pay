6f0008cf5cbfb66babaafe615079d3a08fedfee4
Richard <richytong@gmail.com>
2022-12-07 09:36:43 -0800

    2022 tax brackets

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const TAX_BRACKETS = [
   [0.1, 0],
-  [0.12, 9700],
-  [0.22, 39475],
-  [0.24, 84200],
-  [0.32, 160725],
-  [0.35, 204100],
-  [0.37, 510300],
+  [0.12, 10275],
+  [0.22, 41775],
+  [0.24, 89075],
+  [0.32, 170050],
+  [0.35, 215950],
+  [0.37, 539900],
 ]
 
 const salariesAndBracketAmounts = []
@@ -31,6 +31,11 @@ const calculateFinalIncome = (salary) => {
   })
   const myBracketTaxOwed = (salary - prevCutoff) * myRate
   return salary - subBracketTaxOwed - myBracketTaxOwed
+}
+
+if (process.argv[1] == __filename) {
+  const finalIncome = calculateFinalIncome(process.argv[2]) // node index.js 45000
+  console.log(finalIncome)
 }
 
 module.exports = calculateFinalIncome
